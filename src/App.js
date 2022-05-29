382466d4394bf9d124944b32684ab1ee3e970a3c
faisalk2 <faisal.z.khan5538@gmail.com>
2022-05-29 15:38:10 +0530

    side bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SimpleSidebar from './components/SimpleSidebar';
+
+function App() {
+  return (
+    <div className="App">
+     <SimpleSidebar/>
+    </div>
+  );
+}
+
+export default App;
